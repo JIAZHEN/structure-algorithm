986d5790aa6d5d486417004315a0242fd5471c13
Joe Xie <fdf515@gmail.com>
2018-04-01 10:39:57 +0100

    Ugly implementation of dijkstra alg

--- data-structures/graph/graph.rb
+++ data-structures/graph/graph.rb
@@ -9,72 +9,116 @@ class Graph
     end
   end
 
-  attr_accessor :nodes_by_id, :edges
+  attr_accessor :nodes_by_id, :edges, :dijkstra
 
   def initialize
     @nodes_by_id = {}
-    @edges = Hash.new(Float::INFINITY)
+    @edges = {}
+    # key is destination node, value is a hash with length and previous node
+    @dijkstra = {}
   end
 
   def get_node(id)
     nodes_by_id[id]
   end
 
-  def get_edge(source_id, destination_id)
-    edges["#{source_id}-#{destination_id}"]
+  def get_edge(src_id, desc_id)
+    edges["#{src_id}-#{desc_id}"]
   end
 
   def add_node(*nodes)
     nodes.map { |node| nodes_by_id[node.id] = node }
   end
 
-  def add_edge(source_id, destination_id, length: Float::INFINITY)
-    source, destination = get_nodes(source_id, destination_id)
-    source.adjacents << destination
-    edges["#{source_id}-#{destination_id}"] = length
-    edges["#{destination_id}-#{source_id}"] = length
+  def get_nodes(*node_ids)
+    node_ids.map { |node_id| get_node(node_id) }
   end
 
-  def has_path?(source_id, destination_id, alg: :dfs)
-    source, destination = get_nodes(source_id, destination_id)
+  def add_edge(src_id, desc_id, len:)
+    src, desc = get_nodes(src_id, desc_id)
+    src.adjacents << desc
+    edges["#{src_id}-#{desc_id}"] = len
+    edges["#{desc_id}-#{src_id}"] = len
+  end
+
+  def has_path?(src_id, desc_id, alg: :dfs)
+    src, desc = get_nodes(src_id, desc_id)
+    return false unless src && desc
+
     if alg == :dfs
-      dfs_path(source, destination, [])
+      !!dfs_path(src, desc, [])
     elsif alg == :bfs
-      bfs_path(destination, [], Queue.new.enq(source))
+      !!bfs_path(desc, [], Queue.new.enq(src))
     else
       raise "Unknow path search algorithm"
     end
   end
 
-  private
+  def dfs_path(src, desc, visited)
+    return false if visited.include?(src)
+    visited << src
 
-  def get_nodes(*node_ids)
-    node_ids.map { |node_id| get_node(node_id) }
-  end
-
-  def dfs_path(source, destination, visited)
-    return false if visited.include?(source)
-    visited << source
-
-    if source == destination
+    if src == desc
       visited
     else
-      source.adjacents.each do |adjacent|
-        return visited if dfs_path(adjacent, destination, visited)
+      src.adjacents.each do |adjacent|
+        return visited if dfs_path(adjacent, desc, visited)
       end
     end
   end
 
-  def bfs_path(destination, visited, queue)
+  def bfs_path(desc, visited, queue)
     until queue.empty?
       node = queue.deq
       return false if visited.include?(node)
       visited << node
 
-      if node == destination
-        visited
+      if node == desc
+        return visited
       else
         node.adjacents.each { |adjacent| queue.enq(adjacent) }
+      end
+    end
+  end
+
+  def build_dijkstra!
+    unvisted = nodes_by_id.keys
+    desc_id = unvisted.shift
+    start_len = 0
+    dijkstra[desc_id] = {len: 0, prev: desc_id}
+
+    until unvisted.empty?
+      min_len, min_id = Float::INFINITY, nil
+      nodes_by_id[desc_id].adjacents.each do |src_node|
+        src_id = src_node.id
+        dijkstra[src_id] ||= {len: Float::INFINITY, prev: src_id}
+
+        if (len = get_edge(src_id, desc_id)) && (start_len + len) < dijkstra[src_id][:len]
+          dijkstra[src_id] = {len: (start_len + len), prev: desc_id}
+          if min_len > (start_len + len)
+            min_len = start_len + len
+            min_id = src_id
+          end
+        end
+      end
+      start_len = min_len
+      desc_id = unvisted.delete(min_id) || unvisted.pop
+    end
+  end
+
+  def shortest_path(desc_id, prev_id)
+    path, length = [], 0
+
+    (return [path, length]) if desc_id == prev_id
+
+    while record = dijkstra[desc_id]
+      path << desc_id
+
+      if record[:prev] == prev_id
+        return (path << prev_id), length
+      else
+        length += dijkstra[desc_id][:len]
+        desc_id = dijkstra[desc_id][:prev]
       end
     end
   end
--- spec/data-structures/graph_spec.rb
+++ spec/data-structures/graph_spec.rb
@@ -16,13 +16,13 @@ describe Graph do
     it "stores the nodes" do
       nodes = %w(A B C D E).map { |id| described_class::Node.new(id) }
       graph.add_node(*nodes)
-      graph.add_edge('A', 'B', length: 6)
-      graph.add_edge('A', 'D', length: 1)
-      graph.add_edge('B', 'C', length: 5)
-      graph.add_edge('D', 'B', length: 2)
-      graph.add_edge('D', 'E', length: 1)
-      graph.add_edge('E', 'B', length: 2)
-      graph.add_edge('E', 'C', length: 5)
+      graph.add_edge('A', 'B', len: 6)
+      graph.add_edge('A', 'D', len: 1)
+      graph.add_edge('B', 'C', len: 5)
+      graph.add_edge('D', 'B', len: 2)
+      graph.add_edge('D', 'E', len: 1)
+      graph.add_edge('E', 'B', len: 2)
+      graph.add_edge('E', 'C', len: 5)
       expect(graph.edges).to eq({
         "A-B" => 6,
         "B-A" => 6,
@@ -39,6 +39,77 @@ describe Graph do
         "E-C" => 5,
         "C-E" => 5
       })
+    end
+  end
+
+  context "about path" do
+    let(:nodes) { %w(A B C D E).map { |id| described_class::Node.new(id) } }
+    let(:nodes_hash) { nodes.map{|n| [n.id,n]}.to_h }
+
+    before do
+      graph.add_node(*nodes)
+      graph.add_edge('A', 'B', len: 6)
+      graph.add_edge('A', 'D', len: 1)
+      graph.add_edge('B', 'C', len: 5)
+      graph.add_edge('D', 'B', len: 2)
+      graph.add_edge('D', 'E', len: 1)
+      graph.add_edge('E', 'B', len: 2)
+      graph.add_edge('E', 'C', len: 5)
+    end
+
+    context "#has_path?" do
+      it "returns nil when destination not found" do
+        expect(graph.has_path?('A', 'G')).to be_falsey
+      end
+
+      it "returns nil when source not found" do
+        expect(graph.has_path?('H', 'A')).to be_falsey
+      end
+
+      it "returns true when path is found" do
+        expect(graph.has_path?('B', 'A')).to be_truthy
+      end
+    end
+
+    context "#dfs_path" do
+      it "returns the correct path" do
+        source, destination = graph.get_nodes('A', 'B')
+        expect(graph.dfs_path(source, destination, [])).to eq(nodes_hash.values_at('A', 'B'))
+
+        source, destination = graph.get_nodes('A', 'C')
+        expect(graph.dfs_path(source, destination, [])).to eq(nodes_hash.values_at('A', 'B', 'C'))
+      end
+    end
+
+    context "#bfs_path" do
+      it "returns the correct path" do
+        source, destination = graph.get_nodes('A', 'B')
+        expect(graph.bfs_path(destination, [], Queue.new.enq(source))).to eq(
+          nodes_hash.values_at('A', 'B'))
+
+        source, destination = graph.get_nodes('A', 'C')
+        expect(graph.bfs_path(destination, [], Queue.new.enq(source))).to eq(
+          nodes_hash.values_at('A', 'B', 'D', 'C'))
+      end
+    end
+
+    context "#dijkstra" do
+      before { graph.build_dijkstra! }
+
+      it "builds the dijkstra table" do
+        expect(graph.dijkstra).to eq({
+          'A' => {len: 0, prev: 'A'},
+          'B' => {len: 3, prev: 'D'},
+          'C' => {len: 7, prev: 'E'},
+          'D' => {len: 1, prev: 'A'},
+          'E' => {len: 2, prev: 'D'}
+        })
+      end
+
+      it "find the correct shortest path with length" do
+        expect(graph.shortest_path('B', 'B')).to eq([[], 0])
+        expect(graph.shortest_path('B', 'A')).to eq([['B', 'D', 'A'], 3])
+      end
     end
   end
 end
